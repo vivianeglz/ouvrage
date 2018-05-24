63ff53e0c36cc34ac987074472f823ee334411a2
maxencedewaele <maxencedewaele@live.fr>
2018-05-24 13:25:29 +0200

    Update styling + structure profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -45,46 +45,51 @@ export default class Profile extends React.Component {
           <h1 className="title">{firstName} {lastName}</h1>
           <EditBtn handleClick={this.handleUpdating} />
         </div>
-        <div className="wrap-content">
-          {this.state.edit ? (
-            <form className="content" onSubmit={this.handleSubmit}>
-              <input name="id_student" type="hidden" value={id}/>
-              <div>
-                <label>Prénom</label>
-                <input type="text" id="first_name" name="first_name" defaultValue={firstName}/>
-              </div>
-              <div>
-                <label>Nom de famille</label>
-                <input type="text" id="last_name" name="last_name" defaultValue={lastName}/>
-              </div>
-              <div>
-                <label>Username</label>
-                <input type="text" id="username" name="username" defaultValue={username}/>
-              </div>
-              <div>
-                <label>Adresse e-mail</label>
-                <input type="email" id="email" name="email" defaultValue={email}/>
-              </div>
-              <div>
-                <label>Date de naissance</label>
-                <input type="date" id="birthdate" name="birthdate" defaultValue={this.state.birthdate}/>
-              </div>
-              <input type="submit" value="Submit"/>
-            </form>
-          ) : (
-            <div>
-              <div className="avatar">
-                <img src={avatar} alt="avatar"/>
-              </div>
-              <div className="content">
-                <p>{firstName}  {lastName}</p>
-                <p>{username}</p>
-                <p>{email}</p>
-                <p>{this.state.birthdate}</p>
-              </div>
+        {this.state.edit ? (
+          <div className="wrap-content">
+            <div className="avatar">
+              <img src={avatar} alt="avatar"/>
             </div>
-          )}
-        </div>
+            <div className="content">
+              <form onSubmit={this.handleSubmit}>
+                <input name="id_student" type="hidden" value={id}/>
+                <div className="wrap-input">
+                  <label>Prénom</label>
+                  <input type="text" id="first_name" name="first_name" defaultValue={firstName}/>
+                </div>
+                <div className="wrap-input">
+                  <label>Nom de famille</label>
+                  <input type="text" id="last_name" name="last_name" defaultValue={lastName}/>
+                </div>
+                <div className="wrap-input">
+                  <label>Username</label>
+                  <input type="text" id="username" name="username" defaultValue={username}/>
+                </div>
+                <div className="wrap-input">
+                  <label>Adresse e-mail</label>
+                  <input type="email" id="email" name="email" defaultValue={email}/>
+                </div>
+                <div className="wrap-input">
+                  <label>Date de naissance</label>
+                  <input type="date" id="birthdate" name="birthdate" defaultValue={this.state.birthdate}/>
+                </div>
+                <input type="submit" value="Submit"/>
+              </form>
+            </div>
+          </div>
+        ) : (
+          <div className="wrap-content">
+            <div className="avatar">
+              <img src={avatar} alt="avatar"/>
+            </div>
+            <div className="content">
+              <p>{firstName}  {lastName}</p>
+              <p>{username}</p>
+              <p>{email}</p>
+              <p>{this.state.birthdate}</p>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
